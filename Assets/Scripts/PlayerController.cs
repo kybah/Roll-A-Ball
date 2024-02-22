d1712beeffb31959b650293e5a65bbe1f479de66
kybah <zacdoherty2000@gmail.com>
2024-02-22 11:27:52 +1100

    Pick Ups Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {  
     public float speed = 1f;
-    private Rigidbody rb; 
+    private Rigidbody rb;
+    public int pickUpCount; 
 
     // Start is called before the first frame update
     void Start()
     {
        rb = GetComponent<Rigidbody>();
+        //Get the number of pick ups in our scene
+        pickUpCount = GameObject.FindGameObjectsWithTag("Pick Up").Length;
+        //Run the check pick ups functon
+        CheckPickUps();
     }
 
     // Update is called once per frame
@@ -21,5 +26,26 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(movehorizontal, 0, moveVertical);
         rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Pick Up")
+        {
+            Destroy(other.gameObject);
+            pickUpCount--;
+            CheckPickUps();
+        }
+        
+
+
+    }
+
+ void CheckPickUps()
+    {
+        print("Pick Ups Left: " + pickUpCount);
+        if(pickUpCount == 0)
+        {
+            print("congratulations");
+        }
     }
 }
